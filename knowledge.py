08d7bbc1ec012cf8cdc2d9dc3712a0106b25f19d
paineliu <liutingchao@hotmail.com>
2024-08-13 08:32:08 +0800

    add phrase

--- knowledge.py
+++ knowledge.py
@@ -2,6 +2,7 @@ from collections import defaultdict
 import json
 import os
 
+
 def make_chaizi_knowledge(info_pathname, chaizi_filename, phrase_filename, knowledge_filename):
     map_han_pinyin = {}
     f_pinyin = open(os.path.join(info_pathname, 'pinyin.txt'), encoding='utf-8')
@@ -19,7 +20,6 @@ def make_chaizi_knowledge(info_pathname, chaizi_filename, phrase_filename, knowl
         radical = items[1].replace(' ', '')
         map_han_radical[han] = [radical]
 
-        print(items)
         # map_han_pinyin[items[3]] = map_han_pinyin[1]
 
     f_chaizi = open(os.path.join(info_pathname, 'chaizi-utf8.csv'), encoding='utf-8')
@@ -71,14 +71,30 @@ def make_chaizi_knowledge(info_pathname, chaizi_filename, phrase_filename, knowl
             map_rare_han[han]['radical'].append(radical)
         map_radidal_han[radical] = han
 
+    alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"  # 定义字母表
+    
     f_phrase = open(phrase_filename, encoding='utf-8')
     for each in f_phrase:
         items = each.strip().split('\t')
         phrase = items[0]
         freq = int(items[1])
-        for han in phrase:
-            if han in map_rare_han:
-                map_rare_han[han]['phrases'][phrase] = freq
+        valid = True
+        for item in phrase:
+            if item in alphabet:
+                valid = False
+                # print(phrase)
+                break
+        
+        if valid:
+            for item in ['多大', '身高', '资料','新闻','天气','地图', '邮编', '吧', '写真', '的', '诗词','演员','几岁', '试婚', '近况','背景', '违纪','记者', '近况', '小说','贴吧', '帖吧', '男排', '淘汰', "怎么", "拼音","读音","什么", "组词", "邮件","简介", "照片",'意思','博客','图片','微博','简历','官网', '直播','身价']:
+                if item in phrase:
+                    valid = False
+                    break
+
+        if valid:
+            for han in phrase:
+                if han in map_rare_han:
+                    map_rare_han[han]['phrases'][phrase] = freq
     f = open(knowledge_filename, 'w', encoding='utf-8')
     json.dump(map_rare_han, f, ensure_ascii=False, indent=4)
 
@@ -90,34 +106,34 @@ def make_normal_knowledge(info_pathname, normal_filename, phrase_filename, knowl
         if len(items) == 4:
             map_han_pinyin[items[3]] = items[1]
 
-    map_han_radical = {}
+    # map_han_radical = {}
     
-    f_normal = open(os.path.join(info_pathname, 'chaizi-utf8.txt'), encoding='utf-8')
-    for each in f_normal:
-        items = each.strip().split('\t')
-        han = items[0]
-        radical = items[1].replace(' ', '')
-        map_han_radical[han] = [radical]
+    # f_normal = open(os.path.join(info_pathname, 'chaizi-utf8.txt'), encoding='utf-8')
+    # for each in f_normal:
+    #     items = each.strip().split('\t')
+    #     han = items[0]
+    #     radical = items[1].replace(' ', '')
+    #     map_han_radical[han] = [radical]
 
-        print(items)
-        # map_han_pinyin[items[3]] = map_han_pinyin[1]
+    #     print(items)
+    #     # map_han_pinyin[items[3]] = map_han_pinyin[1]
 
-    f_normal = open(os.path.join(info_pathname, 'chaizi-utf8.csv'), encoding='utf-8')
-    for each in f_normal:
-        items = each.strip().split(',')
-        han = items[0]
-        radicals = []
-        radical1 = items[2].replace(' ', '')
-        radical2 = items[3].replace(' ', '')
-        radical3 = items[4].replace(' ', '')
-        if len(radical1) > 0:
-            radicals.append(radical1)
-        if len(radical2) > 0:
-            radicals.append(radical2)
-        if len(radical3) > 0:
-            radicals.append(radical3)
-        if han not in map_han_radical:
-            map_han_radical[han] = radicals
+    # f_normal = open(os.path.join(info_pathname, 'chaizi-utf8.csv'), encoding='utf-8')
+    # for each in f_normal:
+    #     items = each.strip().split(',')
+    #     han = items[0]
+    #     radicals = []
+    #     radical1 = items[2].replace(' ', '')
+    #     radical2 = items[3].replace(' ', '')
+    #     radical3 = items[4].replace(' ', '')
+    #     if len(radical1) > 0:
+    #         radicals.append(radical1)
+    #     if len(radical2) > 0:
+    #         radicals.append(radical2)
+    #     if len(radical3) > 0:
+    #         radicals.append(radical3)
+    #     if han not in map_han_radical:
+    #         map_han_radical[han] = radicals
 
     map_han_level = {}
     for i in range(1, 4):
@@ -157,5 +173,5 @@ def make_normal_knowledge(info_pathname, normal_filename, phrase_filename, knowl
     json.dump(map_rare_han, f, ensure_ascii=False, indent=4)
 
 if __name__ == '__main__':    
-    make_chaizi_knowledge('./info', './output/result_chaizi_freq.txt', './output/result_phrase.txt', './output/result_chaizi_knowledge.json')
-    make_normal_knowledge('./info', './output/result_single_word.txt', './output/result_phrase.txt', './output/result_single_knowledge.json')
+    make_chaizi_knowledge('./info', './output/result_chaizi_freq.txt', './output/result_chaizi_phrase.txt', './output/result_chaizi_knowledge.json')
+    make_normal_knowledge('./info', './output/result_single_word.txt', './output/result_chaizi_phrase.txt', './output/result_single_knowledge.json')
--- phrase.py
+++ phrase.py
@@ -1,164 +1,49 @@
 from collections import defaultdict
 import math
+
 import os
-g_map = defaultdict(float)
 
-g_words = [
-    '垚',
-    '燚',
-    '堃',
-    '焱',
-    '昶',
-    '昱',
-    '犇',
-    '赟',
-    '翊',
-    '燊',
-    '靳',
-    '晟',
-    '骉',
-    '仝',
-    '淼',
-    '旻',
-    '囡',
-    '婧',
-    '煜',
-    '幢',
-    '砼',
-    '臻',
-    '喆',
-    '歆',
-    '夯',
-    '翀',
-    '頔',
-    '騳',
-    '昝',
-    '圩',
-    '艮',
-    '泵',
-    '彧',
-    '尛',
-    '斛',
-    '岑',
-    '黔',
-    '槑',
-    '昕',
-    '趸',
-    '曌',
-    '柘',
-    '灏',
-    '龘',
-    '阚',
-    '怼',
-    '颢',
-    '聿',
-    '珩',
-    '梓',
-    '珏',
-    '疃',
-    '晔',
-    '尕',
-    '淦',
-    '寅',
-    '咲',
-    '颉',
-    '邑',
-    '廿',
-    '訾',
-    '辊',
-    '羴',
-    '鋆',
-    '覃',
-    '矗',
-    '阜',
-    '叒',
-    '沂',
-    '晁',
-    '嬲',
-    '泺',
-    '甦',
-    '劼',
-    '骅',
-    '鱻',
-    '罡',
-    '轶',
-    '灬',
-    '厝',
-    '熠',
-    '暨',
-    '舸',
-    '乜',
-    '酉',
-    '昇',
-    '喀',
-    '姝',
-    '弋',
-    '槎',
-    '滇',
-    '尹',
-    '烨',
-    '罘',
-    '尧',
-    '崟',
-    '汝',
-    '兢',
-    '岫',
-    '毳',]
+def make_phrase(corpus_pathname, word_filename, phrase_filename):
 
-for file in [
-    'E:/data_utf8/20150000.txt', 
-    'E:/data_utf8/20150001.txt', 
-    'E:/data_utf8/20150002.txt', 
-    'E:/data_utf8/20150003.txt', 
-    'E:/data_utf8/20150004.txt', 
-    'E:/data_utf8/20150005.txt', 
-    'E:/data_utf8/20150006.txt', 
-    'E:/data_utf8/20150007.txt', 
-    'E:/data_utf8/20150008.txt', 
-    'E:/data_utf8/20150009.txt', 
-    'E:/data_utf8/20150010.txt', 
-    'E:/data_utf8/20150011.txt', 
-    'E:/data_utf8/20150012.txt', 
-    'E:/data_utf8/20150013.txt', 
-    'E:/data_utf8/20150014.txt', 
-    'E:/data_utf8/20150015.txt', 
-    'E:/data_utf8/20150016.txt', 
-    'E:/data_utf8/20150017.txt', 
-    'E:/data_utf8/20150018.txt', 
-    'E:/data_utf8/20150019.txt', 
-    'E:/data_utf8/20150020.txt', 
-    'E:/data_utf8/20150021.txt', 
-    'E:/data_utf8/20150022.txt', 
-    'E:/data_utf8/20150023.txt', 
-    'E:/data_utf8/20150024.txt', 
-    'E:/data_utf8/20150025.txt', 
-    'E:/data_utf8/20150026.txt', 
-    'E:/data_utf8/20150027.txt', 
-    'E:/data_utf8/20150028.txt', 
-    'E:/data_utf8/20150029.txt', 
-    'E:/data_utf8/20150030.txt', 
-    ]:
-    f = open(file, encoding='utf_8')
-    print(file)
+    f = open(word_filename, encoding='utf_8')
+    g_words = {}
+    g_phrase = defaultdict(float)
+
     for each in f:
-        each = each.rstrip()
-        items = each.split('\t')
-        if (int(items[1]) <= 5):
-            break
-        for w in g_words:
-            if (w in items[0]):
-                if len(items[0]) <= 5:
-                    freq = math.sqrt(int(items[1]))
-                    g_map[items[0]] += freq
+        items = each.strip().split()
+        g_words[items[0]] = int(items[1])
+        
+    paths = os.walk(corpus_pathname)
+    file_total  = 0
+    for path, dir_lst, file_lst in paths:
+        for file_name in file_lst:
+            full_name = os.path.join(path, file_name)
+            print(full_name)
+            f = open(full_name, encoding='utf_8')
+            file_total += 1
+            for each in f:
+                each = each.rstrip()
+                items = each.split('\t')
+                if (int(items[1]) <= 5):
+                    break
+                for w in items[0]:
+                    if w in g_words:
+                        if len(items[0]) > 1 and len(items[0]) < 5:
+                            freq = math.sqrt(int(items[1]))
+                            g_phrase[items[0]] += freq
+            
+    g_list = sorted(g_phrase.items(), key = lambda x:x[1], reverse = True)
+    total = 0
+    f_out = open(phrase_filename, mode='w', encoding='utf-8')
+    for each in g_list:
+        total += 1
+        freq = int(each[1] * each[1] / file_total)
+        if (total == 1 or (total % 1000 == 0)):
+            print(total, each[0], freq)
+        f_out.write('{}\t{}\n'.format(each[0], freq))
+    f_out.close()
 
-g_list = sorted(g_map.items(), key = lambda x:x[1], reverse = True)
-total = 0
-os.makedirs('output', exist_ok=True)
-f_out = open('output/result_phrase.txt', mode='w', encoding='utf-8')
-for each in g_list:
-    total += 1
-    freq = int(each[1] * each[1] / 31)
-    if (total == 1 or (total % 1000 == 0)):
-        print(total, each[0], freq)
-    f_out.write('{}\t{}\n'.format(each[0], freq))
-f_out.close()
+    pass
+
+if __name__ == '__main__':
+    make_phrase('E:/data_utf8', './output/result_chaizi_word.txt', './output/result_chaizi_phrase.txt')
